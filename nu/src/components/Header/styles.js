15923980c569b0c5c73bc4e042bb60e2421925a9
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-02-07 17:58:03 -0300

    commit ajuste branch

--- nu/src/components/Header/styles.js
+++
@@ -1,8 +0,0 @@
-import {StyleSheet} from 'react-native';
-import styled from 'styled-components/native';
-
-export const Container = styled.View`
-    alignItens: center;
-    padding: 40px 0 30px;
-
-`;
