cc34b40dddde69b7d9da5953405fade5e8bc554f
Gabriel dos Reis Bispo de Morais <gabriel.morais@implantainformatica.com.br>
2020-02-07 13:56:33 -0300

    Ajustando projeto

---
+++ nu/src/components/Header/styles.js
@@ -0,0 +1,8 @@
+import {StyleSheet} from 'react-native';
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    alignItens: center;
+    padding: 40px 0 30px;
+
+`;
--- nu/src/pages/Main/index.js
+++ nu/src/pages/Main/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Header from '../../components/Header';
 import { Container } from './styles';
 
-function Main(){
+export default function Main(){
     return (
-        <Container /> 
+        <>  
+            <Container>
+                {/* <Header /> */}
+            </Container>
+        </>
     );
 }
-
-export default Main;
